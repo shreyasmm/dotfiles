8b509e6143b805c0aa805e5c4badfd2e1e1877df
Shreyas MM <2624635+shreyasmm@users.noreply.github.com>
2020-07-17 22:10:57 +0530

    added brew dependencies

--- shell/packages-install.sh
+++ shell/packages-install.sh
@@ -1,22 +1,20 @@
 #!/bin/sh
 
+# Brew Dependencies
+sudo apt-get install build-essential curl file git
 
-# Utilites
-apt install 
+# Shell Utilites
 apt install tmux
 apt install zsh
 
-# Compliling
-apt install build-essential
-
 # Rails Installation
-sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev 
+# sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev 
 
 # Snaps
-sudo snap install vlc
-sudo snap install telegram-desktop
-sudo snap install code --classic
-sudo snap install postman
-sudo snap install sublime-text --classic
-sudo snap install slack --classic
-sudo snap install skype --classic
+# sudo snap install vlc
+# sudo snap install telegram-desktop
+# sudo snap install code --classic
+# sudo snap install postman
+# sudo snap install sublime-text --classic
+# sudo snap install slack --classic
+# sudo snap install skype --classic
