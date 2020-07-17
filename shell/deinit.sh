70f4aa0da18d943f09a8a92d222d8a0c65a2a85c
Shreyas MM <2624635+shreyasmm@users.noreply.github.com>
2020-07-18 00:27:14 +0530

    Rename exit-run.sh to deinit.sh

---
+++ shell/deinit.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+code --list-extensions > "$HOME/.dotfiles/vscode/installed_vs_extensions"
--- shell/exit-run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-code --list-extensions > "$HOME/.dotfiles/vscode/installed_vs_extensions"
