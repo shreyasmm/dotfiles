4575e7f6d755a46b82ea1bcf397c9a75b94af8fa
Shreyas MM <2624635+shreyasmm@users.noreply.github.com>
2020-07-20 16:13:28 +0530

    Update alias.sh

--- shell/alias.sh
+++ shell/alias.sh
@@ -4,11 +4,26 @@
 alias week='date +%V'
 alias myip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias webserver="python -m SimpleHTTPServer"
+
 alias pbcopy='xclip -selection clipboard'
 alias pbpaste='xclip -selection clipboard -o'
+
 alias reload=". ~/.zshrc && echo 'ZSH config reloaded from ~/.zshrc'"
 alias c='clear'
 alias ramdisk="sudo mount -t tmpfs tmpfs /home/shrey/Others/ramdisk -o size=2048m"
+
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+alias fd='find . -type d -name'
+alias ff='find . -type f -name'
+
+alias -g H='| head'
+alias -g T='| tail'
+alias -g G='| grep'
+alias -g L="| less"
+
 # fun
 alias starwars="telnet towel.blinkenlights.nl"
 alias nyan='nc -v nyancat.dakko.us 23'
@@ -17,6 +32,5 @@ alias nyan='nc -v nyancat.dakko.us 23'
 alias k='kubectl'
 alias kg='kubectl get'
 
-# Kubernetes Shell Completion
-# ls
 alias ls='lsd'
+
