0fad150a9cad5cbd166d40a3780f7173da8c087f
Shreyas M M <shreyas.mm@verisk.com>
2021-11-23 22:14:22 +0530

    updates

--- shell/alias.sh
+++ shell/alias.sh
@@ -33,7 +33,8 @@ alias nyan='nc -v nyancat.dakko.us 23'
 # Kubernetes
 alias k='kubectl'
 alias kg='kubectl get'
+alias awsssh="aws ssm start-session --target"
 
 alias ls='lsd'
 alias cat='bat'
-
+alias update='npm -g update && brew upgrade && sudo apt update && sudo apt upgrade && flatpak update'
